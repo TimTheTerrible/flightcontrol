1468771dcd05c78421cb6ee055fffbd8986c25ef
Tim Currie <tcurrie@brightcove.com>
2015-10-08 14:20:34 -0400

    Re-arranged everything just because

--- flightcontrol.ino
+++ flightcontrol.ino
@@ -1,3 +1,13 @@
+/*
+ * flightcontrol
+ *
+ * 2015-10-02 - Tim Currie
+ *
+ * An Arduino-based flight control module for large model rockets.
+ * Uses the Adafruit 10DOF combined sensor breakout driving four
+ * servo-mounted winglets to maintain roll-free vertical flight.
+*/
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_L3GD20_U.h>
@@ -12,11 +22,6 @@
 #define SERVO_ROLL0_PIN  11
 #define SERVO_ROLL1_PIN   6
 
-Adafruit_SoftServo servoPitch0;
-Adafruit_SoftServo servoPitch1;
-Adafruit_SoftServo servoRoll0;
-Adafruit_SoftServo servoRoll1;
-   
 /* Assign a unique ID to the sensors */
 Adafruit_10DOF                dof   = Adafruit_10DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
@@ -27,31 +32,55 @@ Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
 /* Update this with the correct SLP for accurate altitude measurements */
 float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
+sensors_event_t gyro_event;
+sensors_event_t accel_event;
+sensors_event_t mag_event;
+sensors_event_t bmp_event;
+sensors_vec_t   orientation;
+    
+Adafruit_SoftServo servoPitch0;
+Adafruit_SoftServo servoPitch1;
+Adafruit_SoftServo servoRoll0;
+Adafruit_SoftServo servoRoll1;
+   
+volatile uint8_t counter = 0;
+SIGNAL(TIMER0_COMPA_vect) {
+
+  counter += 2;
+
+  if (counter >= 20) {
+    counter = 0;
+    servoPitch0.refresh();
+    servoPitch0.refresh();
+    servoRoll0.refresh();
+    servoRoll1.refresh();
+  }
+}
+
 void initSensors()
 {
   /* Enable gyro auto-ranging */
   gyro.enableAutoRange(true);
     
-  if(!gyro.begin())
-  {
+  if(!gyro.begin()) {
     /* There was a problem detecting the L3GD20 ... check your connections */
     Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
     while(1);
   }
-  if(!accel.begin())
-  {
+
+  if(!accel.begin()) {
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
     while(1);
   }
-  if(!mag.begin())
-  {
+
+  if(!mag.begin()) {
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
-  if(!bmp.begin())
-  {
+
+  if(!bmp.begin()) {
     /* There was a problem detecting the BMP180 ... check your connections */
     Serial.println("Ooops, no BMP180 detected ... Check your wiring!");
     while(1);
@@ -73,51 +102,12 @@ void initServos() {
   servoRoll1.write(90);
 }
 
-void setup(void)
-{
-  Serial.begin(9600);
-  Serial.println(F("Adafruit 10 DOF Pitch/Roll/Heading Example")); Serial.println("");
+void debugDump() {
   
-  /* Initialise the sensors */
-  initSensors();
-
-  initServos();
-}
-
-void loop(void)
-{
-  sensors_event_t gyro_event;
-  sensors_event_t accel_event;
-  sensors_event_t mag_event;
-  sensors_event_t bmp_event;
-  sensors_vec_t   orientation;
-    
-  gyro.getEvent(&gyro_event);
-  accel.getEvent(&accel_event);
-  mag.getEvent(&mag_event);
-  bmp.getEvent(&bmp_event);
-  
-  dof.accelGetOrientation(&accel_event, &orientation);
-  dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation);
-  float temperature;
-  bmp.getTemperature(&temperature);
-
-  long pitch0 = map(orientation.pitch, -45,45,0,179);
-  long pitch1 = map(orientation.pitch, 45,-45,0,179);
-  long roll0 = map(orientation.roll, -45,45,0,179);
-  long roll1 = map(orientation.roll, 45,-45,0,179);
-
-  servoPitch0.write(pitch0);
-  servoPitch1.write(pitch1);
-  servoRoll0.write(roll0);
-  servoRoll1.write(roll1);
-
-  if ( millis() % 1000 < 100 ) {
-  
-    Serial.print("pitch0 = "); Serial.println(pitch0);
-    Serial.print("pitch1 = "); Serial.println(pitch1);
-    Serial.print("roll0 = "); Serial.println(roll0);
-    Serial.print("roll1 = "); Serial.println(roll1);
+    Serial.print("servoPitch0.angle = "); Serial.println(servoPitch0.getAngle());
+    Serial.print("servoPitch1.angle = "); Serial.println(servoPitch1.getAngle());
+    Serial.print("servoRoll0.angle = "); Serial.println(servoRoll0.getAngle());
+    Serial.print("servoRoll1.angle = "); Serial.println(servoRoll1.getAngle());
     
     /* Display the results (speed is measured in rad/s) */
     Serial.print("X: "); Serial.print(gyro_event.gyro.x); Serial.print("  ");
@@ -148,21 +138,36 @@ void loop(void)
   }
 }
 
-// We'll take advantage of the built in millis() timer that goes off
-// to keep track of time, and refresh the servo every 20 milliseconds
-// The SIGNAL(TIMER0_COMPA_vect) function is the interrupt that will be
-// Called by the microcontroller every 2 milliseconds
-volatile uint8_t counter = 0;
-SIGNAL(TIMER0_COMPA_vect) {
-  // this gets called every 2 milliseconds
-  counter += 2;
-  // every 20 milliseconds, refresh the servos!
-  if (counter >= 20) {
-    counter = 0;
+void setup(void)
+{
+  Serial.begin(9600);
+  
+  initSensors();
 
-    servoPitch0.refresh();
-    servoPitch0.refresh();
-    servoRoll0.refresh();
-    servoRoll1.refresh();
-  }
+  initServos();
 }
+
+void loop(void)
+{
+  // Read all the sensors...
+  gyro.getEvent(&gyro_event);
+  accel.getEvent(&accel_event);
+  mag.getEvent(&mag_event);
+  bmp.getEvent(&bmp_event);
+  
+  // Calculate some interesting things...
+  dof.accelGetOrientation(&accel_event, &orientation);
+  dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation);
+  float temperature;
+  bmp.getTemperature(&temperature);
+
+  // Adjust the servos...
+  servoPitch0.write(map(orientation.pitch, -45,45,0,179));
+  servoPitch1.write(map(orientation.pitch, 45,-45,0,179));
+  servoRoll1.write(map(orientation.roll, -45,45,0,179));
+  servoRoll0.write(map(orientation.roll, 45,-45,0,179));
+
+  if ( millis() % 1000 < 100 ) {
+    debugDump();
+}
+
