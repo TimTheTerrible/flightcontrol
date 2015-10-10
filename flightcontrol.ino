c2d6e404378b483d39be15b9bcba5c8969a3363d
Tim Currie <tim@dforge.cc>
2015-10-10 10:29:16 -0400

    tweaking stuff

--- flightcontrol.ino
+++ flightcontrol.ino
@@ -16,18 +16,18 @@
 #include <Adafruit_10DOF.h>
 #include <Servo.h>
 
-#define SERVO_PITCH0_PIN   3
-#define SERVO_PITCH1_PIN   4
-#define SERVO_ROLL0_PIN    5
-#define SERVO_ROLL1_PIN    6
+#define SERVO_PITCH0_PIN      3
+#define SERVO_PITCH1_PIN      4
+#define SERVO_ROLL0_PIN       5
+#define SERVO_ROLL1_PIN       6
 
-#define SERVO_MIN_PITCH  -120
-#define SERVO_MAX_PITCH   120
-#define SERVO_MIN_ROLL   -120
-#define SERVO_MAX_ROLL    120
+#define SERVO_MIN_PITCH    -120
+#define SERVO_MAX_PITCH     120
+#define SERVO_MIN_ROLL     -120
+#define SERVO_MAX_ROLL      120
 
-#define SERVO_MIN_ANGLE    0
-#define SERVO_MAX_ANGLE  180
+#define SERVO_MIN_ANGLE       0
+#define SERVO_MAX_ANGLE     180
 
 /* Assign a unique ID to the sensors */
 Adafruit_10DOF                dof   = Adafruit_10DOF();
@@ -37,7 +37,7 @@ Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
 
 /* Update this with the correct SLP for accurate altitude measurements */
-float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA; // 1014.1; //
 float temperature;
 
 sensors_event_t gyro_event;
@@ -50,6 +50,11 @@ Servo servoPitch0;
 Servo servoPitch1;
 Servo servoRoll0;
 Servo servoRoll1;   
+
+int servoPitch0Trim = 3;
+int servoPitch1Trim = 2;
+int servoRoll0Trim  = 2;
+int servoRoll1Trim  = 2;
 
 void debugDump() {
   
@@ -130,11 +135,12 @@ void loop(void)
   bmp.getTemperature(&temperature);
 
   // Adjust the servos...
-  servoPitch0.write(map(orientation.pitch, SERVO_MIN_PITCH,SERVO_MAX_PITCH,0,179));
-  servoPitch1.write(map(orientation.pitch, SERVO_MAX_PITCH,SERVO_MIN_PITCH,0,179));
-  servoRoll1.write(map(orientation.roll, SERVO_MIN_ROLL,SERVO_MAX_ROLL,0,179));
-  servoRoll0.write(map(orientation.roll, SERVO_MAX_ROLL,SERVO_MIN_ROLL,0,179));
+  servoPitch0.write(map(orientation.pitch, SERVO_MIN_PITCH,SERVO_MAX_PITCH,1,180) + servoPitch0Trim);
+  servoPitch1.write(map(orientation.pitch, SERVO_MAX_PITCH,SERVO_MIN_PITCH,1,180) + servoPitch1Trim);
+  servoRoll0.write(map(orientation.roll, SERVO_MIN_ROLL,SERVO_MAX_ROLL,1,180) + servoRoll0Trim);
+  servoRoll1.write(map(orientation.roll, SERVO_MAX_ROLL,SERVO_MIN_ROLL,1,180) + servoRoll1Trim);
 
+  // dump stats once a second
   if ( millis() % 1000 < 100 ) {
     debugDump();
   }
